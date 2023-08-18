edcb28f220e3f0db6836c98039d9f2215187af9b
nawon <dlskdnjs030425@naver.com>
2023-08-18 22:00:12 +0900

    nawon3

--- src/samsung/OnOff_samsung.js
+++ src/samsung/OnOff_samsung.js
@@ -1,19 +1,62 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "../App.css";
 
 function App() {
   const [currentPage, setCurrentPage] = useState(1);
+  const [aiVoiceOn, setAiVoiceOn] = useState(false);
+  const audioRef = useRef(null);
+  const audioSources = {
+    1: "./audio/40.mp3",
+    2: "./audio/41.mp3",
+    3: "./audio/42.mp3",
+    4: "./audio/49.mp3",
+  };
 
   const handleStartClick = () => {
+    if (audioRef.current) {
+      audioRef.current.pause();
+      audioRef.current.src = "";
+    }
     setCurrentPage(2);
   };
 
   const handleNextClick = () => {
     if (currentPage === 2) {
+      if (audioRef.current) {
+        audioRef.current.pause();
+        audioRef.current.src = "";
+      }
       setCurrentPage(3);
     } else if (currentPage === 3) {
+      if (audioRef.current) {
+        audioRef.current.pause();
+        audioRef.current.src = "";
+      }
       setCurrentPage("levelUp");
     }
+  };
+
+  const handleAiVoiceToggle = () => {
+    setAiVoiceOn(!aiVoiceOn);
+
+    if (audioRef.current) {
+      if (!aiVoiceOn && audioSources[currentPage]) {
+        audioRef.current.src = audioSources[currentPage];
+        audioRef.current.play();
+      } else {
+        audioRef.current.pause();
+        audioRef.current.src = "";
+      }
+    }
+  };
+
+  const handlePageChange = (newPage) => {
+    if (audioRef.current) {
+      audioRef.current.pause();
+      audioRef.current.src = ""; // Set audio source to empty string to stop sound
+    }
+    setCurrentPage(newPage);
+    setAiVoiceOn(false);
   };
 
   return (
@@ -24,12 +67,13 @@ function App() {
       <VoiceButton />
       <NavigationBar />
       {renderPage()}
+      <audio ref={audioRef}>
+        <source src="" type="audio/mpeg" />
+      </audio>
     </div>
   );
 
   function VoiceButton() {
-    const [aiVoiceOn, setAiVoiceOn] = useState(false);
-
     return (
       <div className="voice_btn">
         <p>AI 보이스 {aiVoiceOn ? "켜짐" : "꺼짐"}</p>
@@ -38,7 +82,7 @@ function App() {
             type="checkbox"
             id="switch"
             checked={aiVoiceOn}
-            onChange={() => setAiVoiceOn(!aiVoiceOn)}
+            onChange={handleAiVoiceToggle}
           />
           <label htmlFor="switch" className="switch_label">
             <span className="onf_btn"></span>
@@ -99,7 +143,7 @@ function App() {
     switch (currentPage) {
       case 1:
         return (
-          <div id="pages" className="page1">
+          <div className="page1">
             <img src="img/pngegg 1.png" alt="character" />
             <p>안녕하세요.</p>
             <p>
@@ -113,7 +157,7 @@ function App() {
         );
       case 2:
         return (
-          <div id="pages" className="page2">
+          <div className="page2">
             <p style={{ fontSize: "16px" }}>
               조용한 장소에 머물 때 핸드폰을 꺼야 하는 경우가 많죠.
             </p>
@@ -130,7 +174,7 @@ function App() {
         );
       case 3:
         return (
-          <div id="pages" className="page3">
+          <div className="page3">
             <p style={{ fontSize: "16px" }}>
               먼저, 상단 바를 두 번 아래로 내려주세요!
             </p>
@@ -157,9 +201,9 @@ function App() {
             </button>
           </div>
         );
-      case "levelUp":
+      case "level_up_page":
         return (
-          <div id="pages" className="level_up_page">
+          <div className="level_up_page">
             <img src="img/pngegg 1.png" alt="" />
             <p>성공적으로 "휴대폰 켜고 끄기"를 배웠어요!</p>
             <button className="endBtn">학습 종료하기</button>
